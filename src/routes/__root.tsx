126c7792b69ed1c25f3ea04e3a6c25950828a2aa
Tomasz Kielar <git@k1eu.dev>
2026-03-17 23:01:07 +0100

    chore: update to vite 8

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,5 @@
 import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
-
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import Header from '../components/Header'
 
 import appCss from '../styles.css?url'
@@ -40,17 +38,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       <body>
         <Header />
         {children}
-        <TanStackDevtools
-          config={{
-            position: 'bottom-right',
-          }}
-          plugins={[
-            {
-              name: 'Tanstack Router',
-              render: <TanStackRouterDevtoolsPanel />,
-            },
-          ]}
-        />
+        <TanStackRouterDevtools position='bottom-right' />
         <Scripts />
       </body>
     </html>
--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,17 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import { fileURLToPath, URL } from 'url'
+import { defineConfig } from "vite";
 
-import tailwindcss from '@tailwindcss/vite'
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import { fileURLToPath, URL } from "url";
+
+import tailwindcss from "@tailwindcss/vite";
 
 const config = defineConfig({
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
+    tsconfigPaths: true,
   },
-  plugins: [
-    devtools(),
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tailwindcss(),
-    tanstackStart({
-      start: {
-        entry: './frontend/tanstack-server.ts',
-      }
-    }),
-    viteReact(),
-  ],
-})
+  plugins: [tailwindcss(), tanstackStart(), viteReact()],
+});
 
-export default config
+export default config;
