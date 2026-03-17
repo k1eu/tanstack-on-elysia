586f6a59c253a52d291b52bc1a457ccff63f47b5
Tomasz Kielar <git@k1eu.dev>
2026-03-18 00:16:46 +0100

    feat: add libsql and drizzle

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,39 @@
-import Elysia from "elysia";
+import Elysia, { t } from "elysia";
 import { openapi } from "@elysiajs/openapi";
+import { db } from "./database/db";
+import { usersTable } from "./database/schema";
 
 const backend = new Elysia({
   prefix: "/api",
 })
   .use(openapi())
   .get("/hello", () => "Hello from the backend!")
-  .get("/data", () => ({ message: "This is some data from the backend." }));
+  .get("/data", () => ({ message: "This is some data from the backend." }))
+  .get("/users", async () => {
+    const users = await db.select().from(usersTable);
+
+    return users;
+  })
+  .post(
+    "users",
+    async ({ body }) => {
+      const [user] = await db
+        .insert(usersTable)
+        .values([body.data])
+        .returning();
+
+      return user;
+    },
+    {
+      body: t.Object({
+        data: t.Object({
+          name: t.String(),
+          age: t.Number(),
+          email: t.String(),
+        }),
+      }),
+    },
+  );
 
 export { backend };
 export type Backend = typeof backend;
